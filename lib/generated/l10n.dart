0241fa147253bd35faef79335760423ff743724c
Ahmed Sayed <ahmedsayeed233@gmail.com>
2024-11-04 23:55:38 +0200

    updated some files in signup

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -70,30 +70,30 @@ class S {
     );
   }
 
-  /// `At YourSeat, we are committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and safeguard your personal data when you use the YourSeat mobile app. By using the app, you agree to the collection and use of your information in accordance with this policy.\n\n1. How We Use Your Information: The information we collect from you is used for the following purposes:\n   To provide and maintain the app: Ensuring the app functions properly and improving its performance.\n   To process transactions: Handling payments for movie tickets and related services.\n   To offer personalized recommendations: Suggesting movies and cinemas based on your preferences and location.\n   To communicate with you: Sending notifications about your bookings, promotions, or updates to our services.\n   To improve our services: Analyzing app usage data to enhance features and troubleshoot issues.\n\n2. How We Protect Your Data: Your Seat uses a combination of industry-standard security measures to protect your personal information, including:\n   Encryption: We secure your payment data and personal information with encryption protocols such as SSL.\n   Access Restrictions: Only authorized personnel can access your personal data.\n   Ongoing Audits: We regularly review our security practices to ensure your data is safe.\n   While we take every precaution, no security measure is completely foolproof, and we cannot guarantee the total security of your data.\n\n3. Data Sharing and Disclosure: Your Seat does not sell or share your personal information with third parties, except:\n   Service Providers: We work with trusted third-party partners (e.g., payment processors, cloud storage providers) to help deliver our services.\n   Legal Obligations: We may disclose your information if required to do so by law or in response to a valid legal request (e.g., a court order).\n\n4. Your Rights: You have the following rights regarding your personal data:\n   Access: You may request a copy of the personal data we hold about you.\n   Correction: You can request corrections or updates to your data.\n   Deletion: You may request the deletion of your data, subject to legal requirements.\n   Opt-out of Marketing: You can unsubscribe from promotional communications by following the unsubscribe link in our emails or adjusting your app settings.`
+  /// `At YourSeat, we are committed to protecting your privacy...`
   String get privacyPolicyContent {
     return Intl.message(
-      'At YourSeat, we are committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and safeguard your personal data when you use the YourSeat mobile app. By using the app, you agree to the collection and use of your information in accordance with this policy.\n\n1. How We Use Your Information: The information we collect from you is used for the following purposes:\n   To provide and maintain the app: Ensuring the app functions properly and improving its performance.\n   To process transactions: Handling payments for movie tickets and related services.\n   To offer personalized recommendations: Suggesting movies and cinemas based on your preferences and location.\n   To communicate with you: Sending notifications about your bookings, promotions, or updates to our services.\n   To improve our services: Analyzing app usage data to enhance features and troubleshoot issues.\n\n2. How We Protect Your Data: Your Seat uses a combination of industry-standard security measures to protect your personal information, including:\n   Encryption: We secure your payment data and personal information with encryption protocols such as SSL.\n   Access Restrictions: Only authorized personnel can access your personal data.\n   Ongoing Audits: We regularly review our security practices to ensure your data is safe.\n   While we take every precaution, no security measure is completely foolproof, and we cannot guarantee the total security of your data.\n\n3. Data Sharing and Disclosure: Your Seat does not sell or share your personal information with third parties, except:\n   Service Providers: We work with trusted third-party partners (e.g., payment processors, cloud storage providers) to help deliver our services.\n   Legal Obligations: We may disclose your information if required to do so by law or in response to a valid legal request (e.g., a court order).\n\n4. Your Rights: You have the following rights regarding your personal data:\n   Access: You may request a copy of the personal data we hold about you.\n   Correction: You can request corrections or updates to your data.\n   Deletion: You may request the deletion of your data, subject to legal requirements.\n   Opt-out of Marketing: You can unsubscribe from promotional communications by following the unsubscribe link in our emails or adjusting your app settings.',
+      'At YourSeat, we are committed to protecting your privacy...',
       name: 'privacyPolicyContent',
       desc: '',
       args: [],
     );
   }
 
-  /// `1. User Responsibilities: By using this app, you agree to abide by our terms of service and to conduct yourself in a respectful manner. Please refrain from any behavior that disrupts the movie-going experience of others, such as talking loudly, using your phone, or purchasing tickets for unauthorized use. We reserve the right to revoke your access to the app if these terms are violated.\n\n2. User Account Creation: To create an account, you'll need to provide a valid email address and a strong password. Please choose a password that is difficult to guess and avoid using easily identifiable information. Your account data is stored securely and is only accessible by authorized personnel. Security: Choose a strong password that combines letters, numbers, and symbols. Avoid using the same password for multiple accounts. Enable two-factor authentication (if available) for added security. Never share your login information with anyone.\n\n3. Transaction Processing: All transactions are processed securely using [mention payment gateway]. We accept [list of accepted payment methods, e.g., credit cards, debit cards, PayPal]. Additional Information: Please ensure that your payment information is accurate and up-to-date. We reserve the right to cancel orders in case of fraudulent activity or incorrect payment information. For any questions or concerns regarding payments, please contact our customer support team.\n\n4. Ownership of Content: All content within the app, including images, text, videos, and other materials, is the property of [Your Seat] or its licensors. Unless otherwise indicated, you are not permitted to use, reproduce, modify, distribute, or display any content from the app without prior written permission.`
+  /// `1. User Responsibilities: By using this app...`
   String get termsAndConditionsContent {
     return Intl.message(
-      '1. User Responsibilities: By using this app, you agree to abide by our terms of service and to conduct yourself in a respectful manner. Please refrain from any behavior that disrupts the movie-going experience of others, such as talking loudly, using your phone, or purchasing tickets for unauthorized use. We reserve the right to revoke your access to the app if these terms are violated.\n\n2. User Account Creation: To create an account, you\'ll need to provide a valid email address and a strong password. Please choose a password that is difficult to guess and avoid using easily identifiable information. Your account data is stored securely and is only accessible by authorized personnel. Security: Choose a strong password that combines letters, numbers, and symbols. Avoid using the same password for multiple accounts. Enable two-factor authentication (if available) for added security. Never share your login information with anyone.\n\n3. Transaction Processing: All transactions are processed securely using [mention payment gateway]. We accept [list of accepted payment methods, e.g., credit cards, debit cards, PayPal]. Additional Information: Please ensure that your payment information is accurate and up-to-date. We reserve the right to cancel orders in case of fraudulent activity or incorrect payment information. For any questions or concerns regarding payments, please contact our customer support team.\n\n4. Ownership of Content: All content within the app, including images, text, videos, and other materials, is the property of [Your Seat] or its licensors. Unless otherwise indicated, you are not permitted to use, reproduce, modify, distribute, or display any content from the app without prior written permission.',
+      '1. User Responsibilities: By using this app...',
       name: 'termsAndConditionsContent',
       desc: '',
       args: [],
     );
   }
 
-  /// `We’d love to hear from you! Whether you have inquiries, need support, want to share feedback, or simply have suggestions, our team is here to assist you. Please feel free to reach out to us using the form below or via the provided contact details.\nContact Information:\nCustomer Support Email: [support@yourseatapp.com]\nPhone Number: [+123-456-7890]\n\nFeedback and Suggestions:\nWe highly value your input! If you have any suggestions on how we can improve your experience with YourSeat, please share them in the message field of the contact form or send them directly to our email.\n\nConnect with us on our social media channels for the latest updates, promotions, and more:`
+  /// `We’d love to hear from you! Whether you have inquiries...`
   String get contactUsContent {
     return Intl.message(
-      'We’d love to hear from you! Whether you have inquiries, need support, want to share feedback, or simply have suggestions, our team is here to assist you. Please feel free to reach out to us using the form below or via the provided contact details.\nContact Information:\nCustomer Support Email: [support@yourseatapp.com]\nPhone Number: [+123-456-7890]\n\nFeedback and Suggestions:\nWe highly value your input! If you have any suggestions on how we can improve your experience with YourSeat, please share them in the message field of the contact form or send them directly to our email.\n\nConnect with us on our social media channels for the latest updates, promotions, and more:',
+      'We’d love to hear from you! Whether you have inquiries...',
       name: 'contactUsContent',
       desc: '',
       args: [],
@@ -280,10 +280,10 @@ class S {
     );
   }
 
-  /// `I agree with privacy policy`
+  /// `I agree with the privacy policy`
   String get iAgreeWithPrivacyPolicy {
     return Intl.message(
-      'I agree with privacy policy',
+      'I agree with the privacy policy',
       name: 'iAgreeWithPrivacyPolicy',
       desc: '',
       args: [],
@@ -390,10 +390,10 @@ class S {
     );
   }
 
-  /// `Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor sit amet, consectetur adipiscing elit`
+  /// `Lorem ipsum dolor sit amet, consectetur adipiscing elit...`
   String get loremIpsumPlaceholder {
     return Intl.message(
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor sit amet, consectetur adipiscing elit',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
       name: 'loremIpsumPlaceholder',
       desc: '',
       args: [],
@@ -590,10 +590,10 @@ class S {
     );
   }
 
-  /// `Don’t have any account yet?`
+  /// `Don’t have an account?`
   String get don_t_have_account {
     return Intl.message(
-      'Don’t have any account yet?',
+      'Don’t have an account?',
       name: 'don_t_have_account',
       desc: '',
       args: [],
@@ -795,6 +795,246 @@ class S {
     return Intl.message(
       'Error',
       name: 'error',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Registration successful!`
+  String get registerSuccess {
+    return Intl.message(
+      'Registration successful!',
+      name: 'registerSuccess',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Error saving user data.`
+  String get errorSavingUser {
+    return Intl.message(
+      'Error saving user data.',
+      name: 'errorSavingUser',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Already have an account? Sign In`
+  String get alreadyHaveAccount {
+    return Intl.message(
+      'Already have an account? Sign In',
+      name: 'alreadyHaveAccount',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `January`
+  String get january {
+    return Intl.message(
+      'January',
+      name: 'january',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `February`
+  String get february {
+    return Intl.message(
+      'February',
+      name: 'february',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `March`
+  String get march {
+    return Intl.message(
+      'March',
+      name: 'march',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `April`
+  String get april {
+    return Intl.message(
+      'April',
+      name: 'april',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `May`
+  String get may {
+    return Intl.message(
+      'May',
+      name: 'may',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `June`
+  String get june {
+    return Intl.message(
+      'June',
+      name: 'june',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `July`
+  String get july {
+    return Intl.message(
+      'July',
+      name: 'july',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `August`
+  String get august {
+    return Intl.message(
+      'August',
+      name: 'august',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `September`
+  String get september {
+    return Intl.message(
+      'September',
+      name: 'september',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `October`
+  String get october {
+    return Intl.message(
+      'October',
+      name: 'october',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `November`
+  String get november {
+    return Intl.message(
+      'November',
+      name: 'november',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `December`
+  String get december {
+    return Intl.message(
+      'December',
+      name: 'december',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter phone number`
+  String get enterPhoneNumber {
+    return Intl.message(
+      'Enter phone number',
+      name: 'enterPhoneNumber',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter user name`
+  String get enterUsername {
+    return Intl.message(
+      'Enter user name',
+      name: 'enterUsername',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Invalid user name`
+  String get invalidUsername {
+    return Intl.message(
+      'Invalid user name',
+      name: 'invalidUsername',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wrong phone format`
+  String get wrongPhoneFormat {
+    return Intl.message(
+      'Wrong phone format',
+      name: 'wrongPhoneFormat',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter password`
+  String get enterPassword {
+    return Intl.message(
+      'Enter password',
+      name: 'enterPassword',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password criteria`
+  String get passwordCriteria {
+    return Intl.message(
+      'Password criteria',
+      name: 'passwordCriteria',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter confirm password`
+  String get enterConfirmPassword {
+    return Intl.message(
+      'Enter confirm password',
+      name: 'enterConfirmPassword',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wrong confirm password`
+  String get wrongConfirmPassword {
+    return Intl.message(
+      'Wrong confirm password',
+      name: 'wrongConfirmPassword',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ok`
+  String get ok {
+    return Intl.message(
+      'Ok',
+      name: 'ok',
       desc: '',
       args: [],
     );

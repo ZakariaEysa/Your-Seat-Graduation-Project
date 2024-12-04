1de972e1659fbbca5f38e821122b70ff433a1d81
Ahmed Sayed <ahmedsayeed233@gmail.com>
2024-12-04 15:38:21 +0200

    signup page navigate to about us & build edit profile ui & make the logout navigate to login without deleting the account

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';

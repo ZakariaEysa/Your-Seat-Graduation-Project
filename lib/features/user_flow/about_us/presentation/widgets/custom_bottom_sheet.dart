7fba3dd3d5f75c618a83acab5f4ff1ab35b4b5b2
ZakariaEysa <124531014+ZakariaEysa@users.noreply.github.com>
2025-01-30 02:43:51 +0200

    about us bug fixed

--- lib/features/user_flow/about_us/presentation/widgets/custom_bottom_sheet.dart
+++ lib/features/user_flow/about_us/presentation/widgets/custom_bottom_sheet.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../generated/l10n.dart';
 import '../../../../../resources/app_styles_manager.dart';
+import '../../../../../utils/app_logs.dart';
 
-class CustomBottomSheet extends StatelessWidget {
+class CustomBottomSheet extends StatefulWidget {
   final double initialHeight;
   final double maxHeight;
   final double minHeight;
@@ -24,17 +26,24 @@ class CustomBottomSheet extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
+  State<CustomBottomSheet> createState() => _CustomBottomSheetState();
+}
 
+class _CustomBottomSheetState extends State<CustomBottomSheet> {
+  double value =0;
+
+  @override
+  Widget build(BuildContext context) {
+    var lang=S.of(context);
+    var theme = Theme.of(context);
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: 13.w), // تعديل باستخدام ScreenUtil
       child: DraggableScrollableSheet(
-        controller: scrollController,
-        initialChildSize: initialHeight,
-        minChildSize: minHeight,
-        maxChildSize: maxHeight,
+        controller: widget.scrollController,
+        initialChildSize: widget.initialHeight,
+        minChildSize: widget.minHeight,
+        maxChildSize: widget.maxHeight,
         expand: false,
         builder:
             (BuildContext context, ScrollController innerScrollController) {
@@ -44,10 +53,18 @@ class CustomBottomSheet extends StatelessWidget {
               children: [
                 GestureDetector(
                   onVerticalDragUpdate: (details) {
-                    double newSize = scrollController.size -
+                    // AppLogs.debugLog(widget.scrollController.size.toString());
+
+                    double newSize = widget.scrollController.size -
                         (details.delta.dy / MediaQuery.of(context).size.height);
                     if (newSize >= 0 && newSize <= 1) {
-                      scrollController.jumpTo(newSize);
+                      // AppLogs.errorLog(newSize.toString());
+                      value = newSize;
+
+                      widget.scrollController.jumpTo(newSize);
+                      setState(() {
+
+                      });
                     }
                   },
                   child: Container(
@@ -56,43 +73,48 @@ class CustomBottomSheet extends StatelessWidget {
                       vertical: 15.h, // تعديل باستخدام ScreenUtil
                     ),
                     child: Text(
-                      title,
+                      widget.title,
                       style: theme.textTheme.labelLarge!.copyWith(
                           fontSize: 24.sp), // استخدام ScreenUtil لضبط الحجم
                       textAlign: TextAlign.center,
                     ),
                   ),
                 ),
+
                 Expanded(
                   child: SingleChildScrollView(
+
                     controller: innerScrollController,
                     child: Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: 26.w), // تعديل باستخدام ScreenUtil
                       child: Column(
                         children: [
-                          SizedBox(height: 10.h), // تعديل باستخدام ScreenUtil
-                          Text(
-                            content,
+                          SizedBox(height: 30.h), // تعديل باستخدام ScreenUtil
+                          if(value!=0&&value>.15||widget.title!=lang.contactUs)
+                            Text(
+                            widget.content,
                             textAlign: TextAlign.left,
                             style: theme.textTheme.bodyMedium!.copyWith(
                                 fontSize:
                                     12.sp), // استخدام ScreenUtil لضبط الحجم
                           ),
                           SizedBox(height: 20.h), // تعديل باستخدام ScreenUtil
-                          if (bottomWidget != null)
+                          if (widget.bottomWidget != null)
                             Container(
                               alignment: Alignment.center,
                               margin: EdgeInsets.only(
                                   top: 20.h), // تعديل باستخدام ScreenUtil
-                              child: bottomWidget,
+                              child: widget.bottomWidget,
                             ),
-                          SizedBox(height: 120.h), // تعديل باستخدام ScreenUtil
+                          SizedBox(height: 300.h), // تعديل باستخدام ScreenUtil
                         ],
                       ),
                     ),
                   ),
                 ),
+
+
               ],
             ),
           );
